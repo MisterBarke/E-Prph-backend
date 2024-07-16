519ff98ee32b5054e710c89a36f075e6c34575c6
Ousmane <bachirmahbarke@gmail.com>
2024-07-16 18:42:26 +0200

    send mail function on folder assignee

--- src/folders/folders.module.ts
+++ src/folders/folders.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { FoldersService } from './folders.service';
 import { FoldersController } from './folders.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+import { MailModule } from 'src/mail/mail.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, MailModule],
   controllers: [FoldersController],
   providers: [FoldersService],
 })
