aa54c12530789d44eb5734091de00a1dae3f65dd
Misterbarke <bachirmahbarke@gmail.com>
2024-08-24 23:29:32 +0100

    adding database name in database url

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,27 +1,15 @@
 import { PrismaClient, Role } from '@prisma/client';
-import { createClient, SupabaseClient } from '@supabase/supabase-js';
-import { ConfigService } from '@nestjs/config';
+import * as bcrypt from 'bcrypt';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 
 const prisma = new PrismaClient();
 
-const supabaseUrl = process.env.SUPERBASE_PROJECT_URL;
-const supabaseKey = process.env.SUPERBASE_API_KEY;
-
-if (!supabaseUrl || !supabaseKey) {
-  throw new Error('Supabase URL and API Key must be provided.');
-}
-
-// const supabaseClient: SupabaseClient = createClient(supabaseUrl, supabaseKey, {
-//   auth: {
-//     persistSession: false,
-//   },
-// })
-
 async function generatePassword(): Promise<string> {
-  return `123456`; // Remplacer par une génération plus sécurisée en production
+  const password = `123456`; // Remplacer par une génération plus sécurisée en production
+  const saltOrRounds = 10;
+  return await bcrypt.hash(password, saltOrRounds);
 }
 
 async function main() {
@@ -29,43 +17,21 @@ async function main() {
   const position = 'Developer';
   const role: Role = Role.SUDO;
 
-  const password = await generatePassword();
+  const hashedPassword = await generatePassword();
 
-  //   const { data, error } = await supabaseClient.auth.signUp({
-  //     email : email,
-  //     password: password
-  //   });
-  // console.log('data  '+data.user);
+ 
+    const newUser = await prisma.users.create({
+      data: {
+        email,
+        password: hashedPassword,
+        role,
+        position,
+        createdAt: new Date(),
+        phone:''
+      },
+    });
 
-  // if (error) {
-  //   throw new Error(`Error during sign up: ${error.code, error.status}`);
-  // }
-
-  // const { user } = data;
-
-  //   const newUser = await prisma.users.create({
-  //     data: {
-  //       email,
-  //       supabase_id: user.id,
-  //       phone: user.phone ?? '',
-  //       role,
-  //       createdAt: new Date(user.created_at),
-  //     },
-  //   });
-
-  //  await prisma.users.findUnique({
-  //     where: { supabase_id: user.id }
-  //   });
-
-  //   const updatedUser = await prisma.users.update({
-  //     where: { id: newUser.id },
-  //     data: {
-  //       isSignateurDossierAgricole: false,
-  //       position
-  //     },
-  //   });
-
-  //   console.log('User created:', updatedUser);
+    console.log('User created:', newUser);
 }
 
 main()
