7b260105aae5a439e00f71597a2480dfcdb5de45
Misterbarke <bachirmahbarke@gmail.com>
2024-08-27 20:34:08 +0100

    adding ClientFoldersService in FoldeModule

--- src/folders/folders.module.ts
+++ src/folders/folders.module.ts
@@ -3,10 +3,11 @@ import { FoldersService } from './folders.service';
 import { FoldersController } from './folders.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 import { MailModule } from 'src/mail/mail.module';
+import { ClientsFoldersService } from './clientsFolders.service';
 
 @Module({
   imports: [PrismaModule, MailModule],
   controllers: [FoldersController],
-  providers: [FoldersService],
+  providers: [FoldersService, ClientsFoldersService],
 })
 export class FoldersModule {}
