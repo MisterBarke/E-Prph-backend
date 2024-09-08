af7908b9dd0a30abd07078f31e2466f1ef5b5912
Misterbarke <bachirmahbarke@gmail.com>
2024-09-08 19:38:47 +0100

    orderBy createAt in findAll

--- src/folders/folders.service.ts
+++ src/folders/folders.service.ts
@@ -125,7 +125,7 @@ export class FoldersService {
         createdBy: true,
         signateurs: {
           orderBy: {
-            order: 'asc',
+            createdAt: 'asc',
           },
         },
         signatures: true,
