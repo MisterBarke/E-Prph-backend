4776b4b0acd12685346402ff425739b6287b63a2
Ousmane <bachirmahbarke@gmail.com>
2024-09-22 05:51:52 +0000

    change file manager from supabase to aws
    #E-PARAPHEUR-T-147
    Merge-request: E-PARAPHEUR-MR-104
    Merged-by: Barke Ousmane <bachirmahbarke@gmail.com>

--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -2,23 +2,27 @@ import { HttpException, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { SupabaseClient, createClient } from '@supabase/supabase-js';
 import { PrismaService } from '../prisma/prisma.service';
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+import * as path from 'path';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class UploadService {
+  private s3Client: S3Client;
+  private bucketName: string;
   supabaseClient: SupabaseClient;
   constructor(
     private prisma: PrismaService,
     private configService: ConfigService,
   ) {
-    this.supabaseClient = createClient(
-      this.configService.get<string>('supabase.url'),
-      this.configService.get<string>('supabase.key'),
-      {
-        auth: {
-          persistSession: false,
-        },
+   this.s3Client = new S3Client({
+      region: this.configService.get<string>('aws-region'),
+      credentials: {
+        accessKeyId: this.configService.get<string>('aws-access-key-id'),
+        secretAccessKey: this.configService.get<string>('aws-access-key'),
       },
-    );
+    });
+    this.bucketName = this.configService.get<string>('aws-s3-bucket');
   }
   async getFiles(userId: string) {
     const connectedUser = await this.prisma.users.findFirst({
@@ -30,7 +34,7 @@ export class UploadService {
       },
     });
     const folder = await this.prisma.folders.findFirst({
-      where: {
+      where: {  
         departement: {
           id: connectedUser.departement?.id,
         },
@@ -43,6 +47,55 @@ export class UploadService {
     return folder?.documents;
   }
 
+  async uploadFile(
+    file: Express.Multer.File,
+    signature = false,
+    userId?: string,
+  ) {
+    const fileName = `${uuidv4()}${path.extname(file.originalname)}`;
+    const uploadParams = {
+      Bucket: this.bucketName,
+      Key: fileName,
+      Body: file.buffer,
+      ContentType: file.mimetype,
+    };
+
+    try {
+      const result = await this.s3Client.send(new PutObjectCommand(uploadParams));
+
+      const fileUrl = `https://${this.bucketName}.s3.${this.configService.get<string>(
+        'aws-region',
+      )}.amazonaws.com/${fileName}`;
+
+      if (signature) {
+        await this.prisma.users.update({
+          where: { id: userId },
+          data: { userSignatureUrl: fileUrl },
+        });
+      } else {
+        const connectedUser = await this.prisma.users.findFirst({ where: { id: userId } });
+        const newDocument = await this.prisma.documents.create({
+          data: {
+            title: file.originalname,
+            url: fileUrl,
+            createdBy: { connect: { id: connectedUser.id } },
+          },
+        });
+        return newDocument;
+      }
+
+      return {
+        url: fileUrl,
+        originalname: file.originalname,
+        mimetype: file.mimetype,
+      };
+    } catch (error) {
+      console.error(error);
+      throw new Error('Failed to upload file to S3');
+    }
+  }
+
+/* 
   async uploadFile(
     file: Express.Multer.File,
     signature = false,
@@ -101,41 +154,6 @@ export class UploadService {
           });
           connectedUser['departement'] = [defaultDepartement];
         }
-        const defaultFolder = await this.prisma.folders.findFirst({
-          where: {
-            departementId: connectedUser?.departement?.id,
-          },
-        });
-        // if (!defaultFolder) {
-        //   defaultFolder = await this.prisma.folders.create({
-        //     data: {
-        //       description: '',
-        //       title: '',
-        //       adress: '',
-        //       departement: {
-        //         connect: {
-        //           id: connectedUser?.departement?.id,
-        //         },
-        //       },
-        //       email: '',
-        //       telephone: '',
-        //       nom: '',
-        //       createdBy: {
-        //         connect: {
-        //           id: connectedUser?.id,
-        //         },
-        //       },
-        //     },
-        //   });
-        // }
-
-        // const rest: any = {};
-        // if (defaultFolder?.id)
-        //   rest.folder = {
-        //     connect: {
-        //       id: defaultFolder?.id,
-        //     },
-        //   };
         const newDocument = await this.prisma.documents.create({
           data: {
             title: file.originalname,
@@ -161,7 +179,9 @@ export class UploadService {
     }
 
     return supabaseResp.error;
-  }
+  } */
+
+
 
   async uploadClientFile(file: Express.Multer.File, userId?: string) {
     const folder = 'dossiers';
